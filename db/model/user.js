cdd4e2cd99441dd9fb3585d1bf03088cad92b308
ARAM <welcome2aram@gmail.com>
2013-11-17 07:45:46 +0530

    Arther/Anbu - registration server side done

---
+++ db/model/user.js
@@ -0,0 +1,43 @@
+
+var md5 = require('MD5');
+var db = require('../db.js');
+
+exports.createUser = function (user) {
+	var that = {};
+	that.firstname = user.firstname;
+	that.lastname = user.lastname;
+	that.emailId = user.emailId;
+	that.mobile = user.mobile;
+	that.password = md5(user.password);
+	that.dob = user.dob;
+	that.gender = user.gender;
+	that.country = user.country;
+	that.state = user.state;
+	that.city = user.city;
+	that.address = user.address;
+	that.pincode = pincode;
+
+	var validateMail = function(error, cb) {
+		db.query("select count(1) as exists from user_details where email like '" + that.emailId + "'", [], function(err, response) {
+			console.log(response);
+			if(response.rows[0].exists > 0) {
+				error.armail = error.armail || [];
+				error.armail.push(1);  // 1 -> Mail id duplication.
+				cb(error);
+			}
+		});
+	}
+
+	that.validateMail = validateMail;
+	return that;
+};
+
+
+exports.validate = function(user, cb) {
+	var errors = {};
+	for(var key in user) {
+		if(user.hasOwnProperty(key) && typeof user[key] == 'function') {
+			user[key](errors, cb);
+		}
+	}
+}
--- routes/user.js
+++ routes/user.js
@@ -1,27 +1,90 @@
 var db = require('../db/db.js');
-/*
-/*
- * GET users listing.
- */
+var user = require('../db/model/user.js');
+var md5 = require('MD5');
 
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
 
 exports.login = function(req, res) {
-	var query = "SELECT password from user_details where email like '" + req.body.email +"'";
+	var query = "SELECT password, email from user_details where email like '" + req.body.email +"'";
 
 	db.query(query, [], function(err, response) {
 		if(err){
 			console.log("Error in password Retrival", err);
+			res.status(401);
+			res.send({error:{arlogin:1}}); //Invalid login
 		}
-		if(response.rows[0] !== undefined) {
-			req.session.hash = req.body.email;
-			res.send(req.session.hash);
+		if(response.rows[0].password !== undefined) {
+			var passhash = md5(response.rows[0].password);
+			req.session.hash = response.rows[0].email;
+			res.send({username:req.body.email, passhash: passhash});
 		}
 	});
 };
 
 exports.registration = function(req, res) {
-	res.render('registration');
+	if(req.session.hash){ //If already logged in
+		res.send({error: {code :1, message: "Already logged In"}, success: false});
+	}else {
+		var requestBody = req.body;
+		var userDetail = createUser(requestBody);
+		user.validate(userDetail, function(error) {
+			if(error === undefined || error === {}){
+				persistUser(userDetail, function(error, data) {
+					if(error){ // If error in store in db
+						res.status(400);
+						res.send({error:{code: 2, message:"Error in registration"}, success: false});
+					}else{
+						res.send({success: true});
+					}
+				});
+			}else{//If any error in user details
+				console.log("Error in user details", error);
+				res.status(400);
+				res.send(error);
+			}
+		});
+	}
 };
+
+var persistUser = function(user, callback) {
+	var query = "INSERT INTO user_details (firstname, lastname, email, password, mobile, dob, country, state, city , address, pincode, gender)VALUES('" 
+		+ user.firstname + "','"
+		+ user.lastname + "','"
+		+ user.emailId + "','"
+		+ user.password + "','"
+		+ user.mobile + "','"
+		+ user.dob + "','"
+		+ user.country + "','"
+		+ user.state + "','"
+		+ user.city + "','"
+		+ user.address + "','"
+		+ user.pincode + "','"
+		+ user.gender + "')";
+
+		db.query(query, [], function(err, data) {
+			if(err){
+				console.log("Error in INSERT");
+			}else{
+				console.log("DATA: ", data);
+			}
+		});
+};
+
+var createUser = function(body) {
+	return user.createUser({
+		emailId : body.mailId, 
+		password : body.password,
+		mobile : body.mobile,
+		dob : body.dob,
+		country : body.country,
+		state : body.state,
+		city : body.city,
+		address : body.address,
+		firstname : body.firstname,
+		lastname : body.lastname,
+		gender : body.gender,
+		pincode : body.pincode		
+	});
+}
