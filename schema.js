eb9622d32367c56e8de02e802abbcc220ff7d4b9
artherg <artherg@thoughtworks.com>
2013-12-05 23:42:23 +0530

    Arther- Added schema.js to create schema on first run

--- schema.js
+++ schema.js
@@ -3,6 +3,8 @@ var pg = require('pg').native
   , client
   , query;
 
+console.log("Connection String : ----> ", connectionString);
+
 client = new pg.Client(connectionString);
 client.connect();
 query = client.query('CREATE TABLE user_details (firstname varchar, lastname  varchar, email varchar(40), password varchar, mobile varchar(12), dob Date, country varchar(40), state varchar(40), city varchar(50), address varchar, pincode integer, gender varchar(10), CONSTRAINT email_primary_key PRIMARY KEY(email), CONSTRAINT mobile_unique_key UNIQUE(mobile))');
